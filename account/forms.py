eecaae4931a7a18bac2fd0a9c6ade6cbe01b3f50
Mosihere <Mostafakhoshhal@yahoo.com>
2023-12-15 01:07:54 +0330

    account app added.

---
+++ account/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}))
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+    confirm_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm Password'}))
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        user = User.objects.filter(username = data)
+        if user.exists():
+            raise ValidationError('This username is already exist!')
+        return data
+
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        user = User.objects.filter(email = data)
+        if user.exists():
+            raise ValidationError('This Email is already in use ...')
+        return data
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password')
+        p2 = cleaned_data.get('confirm_password')
+        if p1 != p2:
+            raise ValidationError('Your password not match !')
+        
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username or Email'}))
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'Password'}))
---
+++ account/views.py
@@ -0,0 +1,53 @@
+from django.contrib import messages
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'Your Registration Complete')
+            return redirect('account:login')
+        
+        else:
+            messages.warning(request, 'Registration Failed.')
+            return redirect('account:register')
+
+
+    else:
+        form = RegisterForm()
+        context = {'form': form}
+        return render(request, 'account/register.html', context)
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                messages.success(request, 'Logged in successfully.')
+                return redirect('todo:todo-list')
+            else:
+                messages.warning(request, 'Wrong Username or Password!')
+                return redirect('account:login')
+    
+    else:
+        form = LoginForm()
+        context = {'form': form}
+        return render(request, 'account/login.html', context)
