d7edd2e34744d0c65e7c297661fd496d463e20e4
Mosihere <Mostafakhoshhal@yahoo.com>
2023-12-17 17:45:16 +0330

    owner field added as user

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -18,11 +18,23 @@ class Todo(models.Model):
         (HIGH_PRIORITY, 'High')
     ]
 
+    IN_PROGRESS_STAUTS = 'I'
+    PENDING_STATUS = 'P'
+    DONE_STATUS = 'D'
+
+    STATUS_CHOICES = [
+        (IN_PROGRESS_STAUTS, 'In Progress'),
+        (PENDING_STATUS, 'Done'),
+        (DONE_STATUS, 'Pending')
+    ]
+
+
     title = models.CharField(max_length=255)
     details = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=MEDIUM_PRIORITY)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDING_STATUS)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
-
