2176f36e5da36ba5ccc7dc8b71ba99b21c773478
Mosihere <Mostafakhoshhal@yahoo.com>
2024-01-03 01:28:34 +0330

    fix small naming issue

--- account/forms.py
+++ account/forms.py
@@ -37,5 +37,5 @@ class RegisterForm(forms.Form):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username or Email'}))
+    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': 'Password'}))
