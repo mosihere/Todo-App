0e6efb47ac1eecc7c6b8863879b3ba55e8d051ce
Mosihere <Mostafakhoshhal@yahoo.com>
2024-01-07 17:57:45 +0330

    4 route added to handle reset pass

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-
-
+from django.contrib.auth import views as auth_views
 app_name = 'account'
 
 urlpatterns = [
     path('register/', views.user_register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.user_profile, name='profile'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
 ]
