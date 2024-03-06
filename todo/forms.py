acb09aff48dc3463d3879e97b0afbc6f775ba52d
Mosihere <Mostafakhoshhal@yahoo.com>
2024-03-06 15:45:55 +0330

    using date picker for due_date field

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
 from .models import Todo
+from django import forms
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'details', 'priority', 'due_date', 'image']
+        widgets = {
+            'due_date': forms.widgets.DateInput(attrs={'type': 'date'})
+        }
