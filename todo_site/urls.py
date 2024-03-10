a0a817bc9aac6de7b3696cb2895d72174f89a22f
Mosihere <Mostafakhoshhal@yahoo.com>
2024-03-10 09:42:53 +0330

    django_debug_toolbar path added

--- todo_site/urls.py
+++ todo_site/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('todo/', include('todo.urls')),
     path('account/', include('account.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
